663e768dbe9967db54565b2585821e6ef08d6b0a
savasafsar <savasafsar3443@gmail.com>
2023-10-19 14:38:55 +0300

    first commit

---
+++ app/src/main/java/com/example/cryptocrazyapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.cryptocrazyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.cryptocrazyapp.ui.theme.CryptoCrazyAppTheme
+import com.example.cryptocrazyapp.view.CryptoDetailScreen
+import com.example.cryptocrazyapp.view.CryptoListScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoCrazyAppTheme {
+                val navController = rememberNavController()
+
+                  NavHost(navController = navController, startDestination = "crypto_list_screen" ) {
+                     composable("crypto_list_screen") {
+                         //CryptoListScreen
+                         CryptoListScreen(navController = navController)
+                     }
+                      composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}",arguments = listOf(
+                          navArgument("cryptoId") {
+                              type = NavType.StringType
+                          },
+                          navArgument("cryptoPrice") {
+                              type = NavType.StringType
+                          }
+                      )) {
+                          val cryptoId = remember {
+                              it.arguments?.getString("cryptoId")
+                          }
+                          val cryptoPrice = remember {
+                              it.arguments?.getString("cryptoPrice")
+                          }
+                          CryptoDetailScreen(
+                              id = cryptoId ?:""
+                             , price = cryptoPrice ?:""
+                              ,navController =navController )
+                          //CryptoDetailScreen
+                      }
+                  }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/cryptocrazyapp/view/CryptoListScreen.kt
@@ -0,0 +1,10 @@
+package com.example.cryptocrazyapp.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CryptoListScreen(navController: NavController) {
+    Text(text = "hello, world")
+}
